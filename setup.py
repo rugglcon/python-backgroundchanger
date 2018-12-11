dc39471ce1881a3949e11d4ed6e19c04fd61d13c
Connor Ruggles <conruggles@gmail.com>
2018-12-10 19:43:07 -0600

    rename folder

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 import setuptools
 
 try:
-    import background_changer
+    import backgroundchanger
 except ImportError:
     print("background_changer requires python 3.5 or greater. Exiting.")
     quit(1)
 
 setuptools.setup(
-        name='background_changer',
-        version=background_changer.config.VERSION,
-        author=background_changer.config.AUTHOR,
-        author_email=background_changer.config.EMAIL,
+        name='backgroundchanger',
+        version=backgroundchanger.config.VERSION,
+        author=backgroundchanger.config.AUTHOR,
+        author_email=backgroundchanger.config.EMAIL,
         description='background changer using unsplash API.',
         license='MIT',
-        url=background_changer.config.URL,
+        url=backgroundchanger.config.URL,
         packages=['background_changer'],
         python_requires='>=3.5',
         zip_safe=False
