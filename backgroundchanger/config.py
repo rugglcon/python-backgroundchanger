b7bdd3bf161f4cbe6251d657aa252c5b31728ff4
Weverson Nascimento <weverson.sn@gmail.com>
2020-10-07 12:06:31 -0300

    Add functions to manager folders and files (#10)
    * Add functions to manager folders and files
    
    * Add strack trace log
    
    * Remove fixed versions
    
    * Fix log strack trace
    
    * Fix requirements files

--- backgroundchanger/config.py
+++ backgroundchanger/config.py
@@ -8,6 +8,6 @@ URL = 'https://github.com/rugglcon/python-backgroundchanger'
 CONFIG_FOLDER = path.join(Path.home(), '.config', 'python-backgroundchanger')
 CONFIG_FILE = path.join(CONFIG_FOLDER, 'unsplash_keys.json')
 CONFIG_DOWNLOAD_FOLDER = path.join(CONFIG_FOLDER, 'photos')
-LOGFILE = path.join(CONFIG_FOLDER, 'backgroundchanger.log')
+LOGFILE = path.join('/tmp/', 'backgroundchanger.log')
 BACKGROUNDS_LOCAL = path.join(Path.home(), '.local', 'share', 'backgrounds')
 IMG_FORMAT = 'png'
---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+
+from os import mkdir
+from shutil import rmtree
+
+@pytest.fixture()
+def temp_folder():
+  mkdir('./tmp/')
+  yield
+  rmtree('./tmp/')
