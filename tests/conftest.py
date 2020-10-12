74ce9d84c3328460df09caf69dc710de0d3bb7bb
Bishwajit Ghosh <arin17bishwa@gmail.com>
2020-10-13 03:05:28 +0530

    reformatted conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,9 @@ import pytest
 from os import mkdir
 from shutil import rmtree
 
+
 @pytest.fixture()
 def temp_folder():
-  mkdir('./tmp/')
-  yield
-  rmtree('./tmp/')
+    mkdir('./tmp/')
+    yield
+    rmtree('./tmp/')
