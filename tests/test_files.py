5a2bdd949528992b2dc0dd166f02f8a566b9fefb
Bishwajit Ghosh <arin17bishwa@gmail.com>
2020-10-13 23:28:47 +0530

    Resolved issue#2 (#22)
    * reformatted the test_files.py to have 4-space indentation
    
    * added the ability to add access key of the user through commandline.
    
    * removed sequences=True as the keyword argument is not supported
    
    * replaced deprecated platform.linux_distribution() with distro.name() using the distro package
    
    * call reload_gala() only if it is elementary OS system.
    
    * added distro==1.5.0 to requirements.txt
    
    * added version info for requests and pywal packages
    
    * made key=None
    
    * made some formatting edits
    
    * used format() instead of f-string as f-string isn't supported in some older python versions
    
    * removed unused import
    
    * unpinned dependencies
    
    * reformatted utils.py to have similar spacing
    
    * made all indentation of 4 spaces
    
    * replaced fstring with format()
    
    * formatted spacing in api.py
    
    * made request() method from Api class a staticmethod
    
    * removed redundant parentheses
    
    * reformatted conftest.py

--- tests/test_files.py
+++ tests/test_files.py
@@ -6,21 +6,23 @@ from os import listdir
 
 @pytest.mark.usefixtures("temp_folder")
 def test_create_folders():
-  folders = ['./tmp/images','./tmp/background']
-  create_folders(folders)
-  
-  assert all(x in listdir('./tmp/') for x in ['background', 'images'])
+    folders = ['./tmp/images', './tmp/background']
+    create_folders(folders)
+
+    assert all(x in listdir('./tmp/') for x in ['background', 'images'])
+
 
 @pytest.mark.usefixtures("temp_folder")
 def test_should_raise_an_error_when_try_to_create_invalid_folder():
-  create_folders(['./tmp/:'])
-  
-  with pytest.raises(Exception) as error:
-    assert 'Not was possible to create folder :' == error.message
+    create_folders(['./tmp/:'])
+
+    with pytest.raises(Exception) as error:
+        assert 'Not was possible to create folder :' == error.message
+
 
 @pytest.mark.usefixtures("temp_folder")
 def test_file_not_exists():
-  open("./tmp/text.txt", "+w").close()
-  
-  assert file_not_exists('./tmp/text.txt') == False
-  assert file_not_exists('./tmp/not_found.txt') == True
+    open("./tmp/text.txt", "+w").close()
+
+    assert file_not_exists('./tmp/text.txt') == False
+    assert file_not_exists('./tmp/not_found.txt') == True
