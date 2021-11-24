8cda7e6f8bcd9bdcd80c2801d1b14c275387697d
bishwajit <arin17bishwa@gmail.com>
2021-11-24 19:39:07 +0530

    Added options to filter the possible images

--- backgroundchanger/__main__.py
+++ backgroundchanger/__main__.py
@@ -48,6 +48,11 @@ def parse_arguments():
     key_group.add_argument('-s', '--secret',
                            help='Provide the Unsplash API secret key')
 
+    filter_group = args.add_argument_group('filter_group')
+    filter_group.add_argument('-c', '--collections', help="Give collection ID(s) to fetch image from.", nargs='+')
+    filter_group.add_argument('-t', '--topics', help="Give topic IDs to fetch image from.", nargs='+')
+    filter_group.add_argument('-q', '--query', help="Give search query to fetch image from.", nargs=1)
+    filter_group.add_argument('--username', help="Limit selection to photos matching a search term.")
     return args.parse_args()
 
 
@@ -66,6 +71,21 @@ def do_wal(photo, do_colors=True):
         utils.reload_gala()
 
 
+def get_filter_params(filter_params) -> dict:
+    filters = {}
+    if filter_params.collections:
+        filters['collections'] = filter_params.collections
+    if filter_params.topics:
+        filters['topics'] = filter_params.topics
+    if filter_params.query:
+        filters['query'] = filter_params.query
+    if filter_params.username:
+        filters['username'] = filter_params.username
+    if 'query' in filters and ('collections' in filters or 'topics' in filters):
+        raise Exception("You can’t use the collections or topics filtering with query parameters in the same request")
+    return filters
+
+
 def main():
     keys = {}
     parsed = parse_arguments()
@@ -73,6 +93,7 @@ def main():
         keys['access_key'] = parsed.access
     if parsed.secret:
         keys['secret_key'] = parsed.secret
+    filter_params = get_filter_params(parsed)
 
     try:
         start_log()
@@ -92,7 +113,7 @@ def main():
     keys = utils.get_keys()
     logging.debug('Got the keys: {}'.format(keys))
     api_object = api.Api(keys)
-    photo = api_object.get_random()
+    photo = api_object.get_random(filter_params)
     do_wal(photo, set_colors)
 
 
