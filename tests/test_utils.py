ed21f772ec719b936dfd336c70dd73555297cd76
bishwajit <arin17bishwa@gmail.com>
2021-12-12 14:14:27 +0530

    reformatting edits

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,13 +8,13 @@ from mock import MagicMock, patch
 from backgroundchanger import utils
 from open_file_mock import MockOpen
 
+
 @patch('backgroundchanger.utils.platform_system')
 class TestExceptions(unittest.TestCase):
     def test_mac_exception(self, mock_platform):
         mock_platform.return_value = 'Darwin'
         self.assertRaises(
             ValueError, utils.get_background_cmd, "./tests/test.png")
-
 
     def test_win_exception(self, mock_platform):
         mock_platform.return_value = 'Windows'
@@ -26,7 +26,7 @@ class TestExceptions(unittest.TestCase):
         mock_distro.return_value = 'Ubuntu'
         mock_platform.return_value = 'Linux'
         res = utils.get_background_cmd("./tests/test.png")
-        assert res[0] in ('gsettings','/usr/bin/gsettings')
+        assert res[0] in ('gsettings', '/usr/bin/gsettings')
 
 
 @patch('backgroundchanger.utils.Tk')
@@ -47,19 +47,19 @@ def test_get_keys():
             "access_key" : "ak",
             "secret_key" : "sk"
         }'''
-        config_file_path = path.join(Path.home(), '.config', 'python-backgroundchanger','unsplash_keys.json')
+        config_file_path = path.join(Path.home(), '.config', 'python-backgroundchanger', 'unsplash_keys.json')
         open_mock.set_read_data_for(path=config_file_path, data=jsonStr)
         keys = utils.get_keys()
-        assert keys['access_key'] =='ak'
-        assert keys['secret_key'] =='sk'
+        assert keys['access_key'] == 'ak'
+        assert keys['secret_key'] == 'sk'
 
 
 @patch('backgroundchanger.utils.Popen')
 def test_reload_gala(mock_popen):
     utils.reload_gala()
     mock_popen.assert_called_once_with(['gala', '-r'],
-        stderr=subprocess.DEVNULL,
-        stdout=subprocess.DEVNULL)
+                                       stderr=subprocess.DEVNULL,
+                                       stdout=subprocess.DEVNULL)
 
 
 def test_copy_file():
@@ -80,6 +80,6 @@ def test_copy_file():
 @patch('backgroundchanger.utils.call')
 def test_change_background(mock_call):
     utils.get_background_cmd = MagicMock()
-    utils.get_background_cmd.return_value = ['dummy','cmd']
+    utils.get_background_cmd.return_value = ['dummy', 'cmd']
     utils.change_background("./tests/test.png")
     mock_call.assert_called_once_with(['dummy', 'cmd'])
